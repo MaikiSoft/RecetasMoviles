e4bacabd71848fdb0450eb5ae05f06b580f03875
SergioPC <sergiolb@hotmail.es>
2025-03-11 16:53:39 -0500

    Minor changes
    Se mejora la forma de renderizado de los instrucciones de las recetas

--- pruebaPantalla/pages/PasosRecetaScreen.js
+++ pruebaPantalla/pages/PasosRecetaScreen.js
@@ -1,14 +1,16 @@
-import { WebView } from 'react-native-webview';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, FlatList, Pressable } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import React, { useCallback, useState } from 'react';
 import YoutubePlayer from 'react-native-youtube-iframe';
+
 
 const PasosRecetaScreen = () => {
     const route = useRoute();
     const { receta } = route.params;
 
     const [playing, setPlaying] = useState(false);
+    const [expandedId, setExpandedId] = useState(null);
+    const instrucciones = receta.strInstructions.split(/\r\n/).filter(item => item.trim() !== '').map((texto, index) => ({ id: (index + 1).toString(), texto }));
 
     const onStateChange = useCallback((state) => {
         if (state === 'ended') {
@@ -16,23 +18,46 @@ const PasosRecetaScreen = () => {
         }
     }, []);
 
+    const renderItem = ({ item }) => (
+        <Pressable
+            onPress={() => setExpandedId(expandedId === item.id ? null : item.id)}
+            style={styles.item}
+        >
+            <Text style={styles.title}>{(item.id)}</Text>
+            {expandedId === item.id && (
+                <View>
+                    {/* <Image source={{ uri: item.image }} style={styles.image} /> */}
+                    <Text style={styles.description}>{item.texto}</Text>
+                </View>
+            )}
+        </Pressable>
+    );
+
     //console.log(receta)
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>¡Bienvenido a la nueva pantalla!</Text>
             <Text style={styles.text}>{receta.strMeal}</Text>
-            <Image source={{ uri: receta.strMealThumb }} style={{ width: 100, height: 100 }}></Image>
-            <View>
-                <Text>video</Text>
-                <YoutubePlayer
-                    height={400}
-                    width={300}
-                    play={playing}
-                    videoId={receta.strYoutube.split("v=")[1]} 
-                    onChangeState={onStateChange}
-                    WebView={WebView}
-                />
+            <View style={styles.imagenContainer}>
+                <Image source={{ uri: receta.strMealThumb }} style={{ width: 200, height: 180 }}></Image>
             </View>
+            <FlatList
+                data={instrucciones}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                ListFooterComponent={
+                    <>
+                        <View>
+                            <YoutubePlayer
+                                height={200}
+                                width={330}
+                                play={playing}
+                                videoId={receta.strYoutube.split("v=")[1]}
+                                onChangeState={onStateChange}
+                            />
+                        </View>
+                    </>
+                }
+            />
         </View>
     )
 }
@@ -42,10 +67,29 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#000',
     },
     text: {
         fontSize: 20,
         fontWeight: 'bold',
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        width: 300,
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    imagenContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingBottom:10
+    },
+    title: {
+        fontSize: 18,
+    },
+    description: {
+        marginTop: 10,
     },
 });
 
