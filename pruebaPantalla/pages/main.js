0ac8c95b0e447999bec6de438175319d4b3e1169
Michael Castillo <maicolestebancastillolopez@gmail.com>
2025-03-11 07:14:03 -0500

    segunda pantalla terminada

---
+++ pruebaPantalla/pages/main.js
@@ -0,0 +1,147 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable, TextInput,ScrollView } from 'react-native';
+import { useState, useEffect } from 'react';
+import { getCategorias, getAreas, getIngredientes } from './services/ApiRecetas';
+
+export default function App() {
+  const [categoria, setCategoria] = useState([]);
+  const [area, setArea] = useState([]);
+  const [ingrediente, setIngrediente] = useState([]);
+  const [selected, setSelected] = useState('');
+  const [verLista, setVerLista] = useState(false);
+
+  useEffect(() => {
+    const fetchCategorias = async () => {
+      const data = await getCategorias();
+      setCategoria(data);
+    };
+    const fetchAreas = async () => {
+      const data = await getAreas();
+      setArea(data);
+    };
+    const fetchIngredientes = async () => {
+      const data = await getIngredientes();
+      setIngrediente(data);
+    };
+    fetchIngredientes();
+    fetchCategorias();
+    fetchAreas();
+  }, []);
+
+  const vistaLista = (select) => {
+    if (selected === select) {
+      setSelected('');
+      setVerLista(false);
+    } else {
+      setSelected(select);
+      setVerLista(true);
+    }
+  };
+
+  const lista = (select) => {
+    let x = [];
+    let text = '';
+    if (select === 'Categoria') {
+      x = categoria;
+      text = 'strCategory';
+    }
+    if (select === 'Area') {
+      x = area;
+      text = 'strArea';
+    }
+    if (select === 'Ingredientes') {
+      x = ingrediente;
+      text = 'strIngredient';
+    }
+
+    return (
+      <>
+        {x.map((cat, index) => (
+          <Pressable key={index} style={styles.options}>
+            <Text>{cat[text]}</Text>
+          </Pressable>
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+      <View style={styles.containerHead}>
+        <TextInput placeholder="Buscar" style={styles.buscar} />
+        <Pressable style={styles.btnSelect}>
+          <Text>Buscar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.containerHead}>
+        <Pressable onPress={() => vistaLista('Categoria')} style={styles.btnSelect}>
+          <Text>Categorias</Text>
+        </Pressable>
+        <Pressable onPress={() => vistaLista('Area')} style={styles.btnSelect}>
+          <Text>Áreas</Text>
+        </Pressable>
+        <Pressable onPress={() => vistaLista('Ingredientes')} style={styles.btnSelect}>
+          <Text>Ingredientes</Text>
+        </Pressable>
+      </View>
+      {verLista && <View style={styles.listas}>{selected !== '' ? lista(selected) : <Text>Cargando...</Text>}</View>}
+      <StatusBar style="auto" />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerHead: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  btnSelect: {
+    backgroundColor: '#DCDDDC',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 10,
+    marginHorizontal: 5,
+    elevation: 3, // Sombra para Android
+    shadowColor: '#000', // Sombra para iOS
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  options: {
+    backgroundColor: '#DCDDDC',
+    padding: 10,
+    borderRadius: 10,
+    width: 200,
+    marginVertical: 2,
+    borderWidth: 1,
+    borderColor: '#000',
+    alignItems: 'center',
+  },
+  listas: {
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buscar: {
+    fontSize: 20,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginVertical: 20,
+    marginHorizontal: 5,
+    height: 40,
+    width: 200,
+    borderRadius: 10,
+  },
+});
---
+++ pruebaPantalla/services/ApiRecetas.js
@@ -0,0 +1,76 @@
+export const getCategorias = async () => {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+        const data = await response.json();
+        return data.meals;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+}
+
+export const getCategoriasFiltro = async (categoria) => {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`);
+        const data = await response.json();
+        return data.meals;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+}
+
+export const getPLato = async (name) => {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+        const data = await response.json();
+        return data.meals;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+};
+
+export const getAreas = async () => {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+        const data = await response.json();
+        return data.meals;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+}
+
+export const getAreasFiltro = async (area) => {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`);
+        const data = await response.json();
+        return data.meals;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+}
+
+export const getIngredientes = async () => {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+        const data = await response.json();
+        return data.meals;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+}
+
+export const getIngredientesFiltro = async (ingrediente) => {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`);
+        const data = await response.json();
+        return data.meals;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+}
